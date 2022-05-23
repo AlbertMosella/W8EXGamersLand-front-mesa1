f2aa6d0fa8a162a0ff21e1f4eea4ea7386c8c47a
josep andreu <josepa45@gmail.com>
2022-05-23 20:51:49 +0200

    Created thunks login register

--- src/redux/thunks/userThunks.js
+++ src/redux/thunks/userThunks.js
@@ -2,11 +2,15 @@ import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { loginActionCreator } from "../features/userSlice";
 
-const loginThunk = (userData) => async (dispatch) => {
+export const loginThunk = (userData) => async (dispatch) => {
   const { data } = await axios.post(
     `${process.env.REACT_APP_API_URL}user/login`,
     userData
   );
   const userInfo = jwtDecode(data.token);
   dispatch(loginActionCreator(userInfo));
+};
+
+export const registerThunk = (userData) => async (dispatch) => {
+  await axios.post(`${process.env.REACT_APP_API_URL}user/register`, userData);
 };
