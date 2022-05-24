9f1acc2b9b970713c694b8b7172c0141cc5bfe3c
Boris <boris@randebrock.com>
2022-05-24 21:31:44 +0200

    Tested userThunks

---
+++ src/redux/thunks/userThunks.test.js
@@ -0,0 +1,24 @@
+import "../../../src/mocks/server";
+import { loginActionCreator } from "../features/userSlice";
+import { loginThunk } from "./userThunks";
+
+jest.mock("jwt-decode", () => () => ({ id: "1234", username: "mockUser" }));
+
+describe("Given the userThunks function", () => {
+  describe("When invoked with a correct username and password", () => {
+    test("Then it should dispatch the loginActionCreator with the username and the id", async () => {
+      const dispatch = jest.fn();
+      const inputData = { username: "mockUser", password: "4321" };
+
+      const userData = { id: "1234", username: "mockUser" };
+
+      const expectedAction = loginActionCreator(userData);
+
+      const thunk = loginThunk(inputData);
+
+      await thunk(dispatch);
+
+      expect(dispatch).toHaveBeenCalledWith(expectedAction);
+    });
+  });
+});
