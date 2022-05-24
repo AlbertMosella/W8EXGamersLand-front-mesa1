05fd70510223745596c5563841d4ba0ced838dec
Cristian Bermúdez <ccbabcn@gmail.com>
2022-05-24 21:52:24 +0200

    Cleared chache on test

--- src/redux/thunks/userThunks.test.js
+++ src/redux/thunks/userThunks.test.js
@@ -5,7 +5,7 @@ import { loginThunk } from "./userThunks";
 jest.mock("jwt-decode", () => () => ({ id: "1234", username: "mockUser" }));
 
 describe("Given the userThunks function", () => {
-  describe("When invoked with a correct username and password", () => {
+  describe("When invoked with a correct username and a password", () => {
     test("Then it should dispatch the loginActionCreator with the username and the id", async () => {
       const dispatch = jest.fn();
       const inputData = { username: "mockUser", password: "4321" };
