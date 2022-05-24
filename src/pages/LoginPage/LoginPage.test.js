876e993cfe7fabab10926dd55e60e5764e2d86be
Abraham Saavedra <abrasm92@gmail.com>
2022-05-24 12:09:11 +0200

    Test snapshot loginPage

--- src/pages/LoginPage/LoginPage.test.js
+++ src/pages/LoginPage/LoginPage.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import reactTestRenderer from "react-test-renderer";
 import store from "../../redux/store/store";
 import LoginPage from "./LoginPage";
 
@@ -20,6 +21,20 @@ describe("Given a LoginPage Component", () => {
       const receivedResult = screen.getByText(expectedResult);
 
       expect(receivedResult).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered", () => {
+    test("Then it alwais show the same structure", () => {
+      const loginPage = reactTestRenderer.create(
+        <Provider store={store}>
+          <BrowserRouter>
+            <LoginPage />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      expect(loginPage).toMatchSnapshot();
     });
   });
 });
