8a63bd598eab22b6d9b8593dba2bfd5b5270ef46
josep andreu <josepa45@gmail.com>
2022-05-24 17:53:35 +0200

    Tested Game component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,19 +1,22 @@
-const Game = ({ name, image, platforms }) => {
+import GameStyles from "./GameStyles";
+
+const Game = ({ game: { name, image, platforms, category } }) => {
   return (
-    <>
+    <GameStyles>
       <img src={image} alt={`portada ${name}`} />
       <h2>{name}</h2>
+      <p>{category}</p>
       <div>
         {platforms.map((platform) => {
           return (
-            platform === "PC" && <span>PC image</span>,
-            platform === "XBOX" && <span>XBOX image</span>,
-            platform === "PLAYSTATION" && <span>PLAYSTATION image</span>,
-            platform === "NINTENDO" && <span>NINTENDO image</span>
+            platform.name === "PC" && <span>PC image</span>,
+            platform.name === "XBOX" && <span>XBOX image</span>,
+            platform.name === "PLAYSTATION" && <span>PLAYSTATION image</span>,
+            platform.name === "NINTENDO" && <span>NINTENDO image</span>
           );
         })}
       </div>
-    </>
+    </GameStyles>
   );
 };
 
