079b32d3d335e1ad58d0a66cc4e17fca06b708c6
Abraham Saavedra <abrasm92@gmail.com>
2022-05-24 12:14:55 +0200

    Test snapshot formPage

--- src/pages/RegisterPage/RegisterPage.test.js
+++ src/pages/RegisterPage/RegisterPage.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import reactTestRenderer from "react-test-renderer";
 import store from "../../redux/store/store";
 import RegisterPage from "./RegisterPage";
 
@@ -20,6 +21,20 @@ describe("Given a RegistersPage Component", () => {
       const receivedResult = screen.getByText(expectedResult);
 
       expect(receivedResult).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered", () => {
+    test("Then it alwais show the same structure", () => {
+      const registerPage = reactTestRenderer.create(
+        <Provider store={store}>
+          <BrowserRouter>
+            <RegisterPage />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      expect(registerPage).toMatchSnapshot();
     });
   });
 });
